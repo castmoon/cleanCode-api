86a15e08083d06500f2249037c13bb5541d7bae2
castmoon <academicgmag@gmail.com>
2021-05-19 12:53:33 -0300

    test: ensure LoginRouter returns 500 if an invalid httpRequest is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -4,12 +4,20 @@
 
 class LoginRouter {
   route(httpRequest) {
+    if (!httpRequest || !httpRequest.body) {
+      return {
+        statusCode: 500,
+      };
+    }
     const { email, password } = httpRequest.body;
     if (!email || !password) {
       return {
         statusCode: 400,
       };
     }
+    return {
+      statusCode: 200,
+    };
   }
 }
 
@@ -36,5 +44,19 @@ describe('login Router', () => {
     };
     const httpResponse = sut.route(httpRequest);
     expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test('should return 500 if no httpRequest is provided', () => {
+    expect.hasAssertions();
+    const sut = new LoginRouter();
+    const httpResponse = sut.route();
+    expect(httpResponse.statusCode).toBe(500);
+  });
+
+  test('should return 500 if httpRequest has no body', () => {
+    expect.hasAssertions();
+    const sut = new LoginRouter();
+    const httpResponse = sut.route({});
+    expect(httpResponse.statusCode).toBe(500);
   });
 });
