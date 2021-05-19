c490842bbcbe61093f5bcdb79985d01af212097a
castmoon <academicgmag@gmail.com>
2021-05-19 12:48:17 -0300

    test: ensure LoginRouter returns 400 if no password is provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,9 +1,11 @@
+/* eslint-disable class-methods-use-this */
 // eslint-disable-next-line jest/consistent-test-it
 // sut = system under testing
 
 class LoginRouter {
   route(httpRequest) {
-    if (!httpRequest.body.email) {
+    const { email, password } = httpRequest.body;
+    if (!email || !password) {
       return {
         statusCode: 400,
       };
@@ -18,6 +20,18 @@ describe('login Router', () => {
     const httpRequest = {
       body: {
         password: 'test_password',
+      },
+    };
+    const httpResponse = sut.route(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+  });
+
+  test('should return 400 if no password is provided', () => {
+    expect.hasAssertions();
+    const sut = new LoginRouter();
+    const httpRequest = {
+      body: {
+        email: 'test@jest.com',
       },
     };
     const httpResponse = sut.route(httpRequest);
