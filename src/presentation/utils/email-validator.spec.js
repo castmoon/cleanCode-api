4cdc66ca0d05286de40938149aadbb44f82cc7ff
castmoon <academicgmag@gmail.com>
2021-05-20 15:24:01 -0300

    refactor: move EmailValidator class to sut

--- src/presentation/utils/email-validator.spec.js
+++ src/presentation/utils/email-validator.spec.js
@@ -8,16 +8,18 @@ class EmailValidator {
   }
 }
 
+const makeSut = () => new EmailValidator();
+
 describe('email Validator', () => {
   test('should return true if validator returns true', () => {
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid('valid@jest.com');
     expect(isEmailValid).toBe(true);
   });
 
   test('should return false if validator returns false', () => {
     validator.isEmailValid = false;
-    const sut = new EmailValidator();
+    const sut = makeSut();
     const isEmailValid = sut.isValid('invalid@jest.com');
     expect(isEmailValid).toBe(false);
   });
