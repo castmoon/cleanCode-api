8f5f4214f4a3e520ecbf79d6dcd453cc4435ff80
castmoon <academicgmag@gmail.com>
2021-05-21 21:39:03 -0300

    refactor: move sut creation to a factory helper method

--- src/presentation/utils/helpers/encrypter.spec.js
+++ src/presentation/utils/helpers/encrypter.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable class-methods-use-this */
 const bcrypt = require('bcrypt');
 
 class Encrypter {
@@ -7,17 +8,19 @@ class Encrypter {
   }
 }
 
+const makeSut = () => new Encrypter();
+
 describe('encrypter', () => {
   test('should return true if bcrypt returns true', async () => {
     expect.hasAssertions();
-    const sut = new Encrypter();
+    const sut = makeSut();
     const isValid = await sut.compare('password', 'hashedPassword');
     expect(isValid).toBe(true);
   });
 
   test('should return false if bcrypt returns false', async () => {
     expect.hasAssertions();
-    const sut = new Encrypter();
+    const sut = makeSut();
     bcrypt.isValid = false;
     const isValid = await sut.compare('invalid_password', 'hashedPassword');
     expect(isValid).toBe(false);
@@ -25,7 +28,7 @@ describe('encrypter', () => {
 
   test('should call bcrypt with correct values', async () => {
     expect.hasAssertions();
-    const sut = new Encrypter();
+    const sut = makeSut();
     await sut.compare('test_password', 'hashedPassword');
     expect(bcrypt.password).toBe('test_password');
     expect(bcrypt.hashedPassword).toBe('hashedPassword');
